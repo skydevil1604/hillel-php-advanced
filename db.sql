c1da74a68cd46e498e3d0c829d93c07dff1af6e9
Valeriia <valeriia.kozhevnikova@develit.se>
2024-02-05 17:30:09 +0200

    added DB

---
+++ db.sql
@@ -0,0 +1,37 @@
+CREATE TABLE customers (
+                           id INT PRIMARY KEY,
+                           name VARCHAR(255) NOT NULL,
+                           phone VARCHAR(255)
+);
+
+CREATE TABLE parks (
+                       id INT PRIMARY KEY,
+                       address VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE cars (
+                      id INT PRIMARY KEY,
+                      park_id INT NOT NULL,
+                      model VARCHAR(255) NOT NULL,
+                      price FLOAT NOT NULL,
+                      FOREIGN KEY (park_id) REFERENCES parks(id)
+);
+
+CREATE TABLE drivers (
+                         id INT PRIMARY KEY,
+                         car_id INT NOT NULL,
+                         name VARCHAR(255) NOT NULL,
+                         phone VARCHAR(255),
+                         FOREIGN KEY (car_id) REFERENCES cars(id)
+);
+
+CREATE TABLE orders (
+                        id INT PRIMARY KEY,
+                        driver_id INT NOT NULL,
+                        customer_id INT NOT NULL,
+                        start TEXT NOT NULL,
+                        finish TEXT NOT NULL,
+                        total FLOAT NOT NULL,
+                        FOREIGN KEY (driver_id) REFERENCES drivers(id),
+                        FOREIGN KEY (customer_id) REFERENCES customers(id)
+);

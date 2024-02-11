8e4c9f9e31b46f2236fc2ab4cc665e0d98611937
Valeriia <valeriia.kozhevnikova@develit.se>
2024-02-11 19:31:41 +0200

    class refactoring to the Single Responsibility principle

---
+++ classes/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+class ProductPropertiesUse
+{
+    public function get(string $name): void {}
+    public function set(string $name, string $value): void {}
+};
+
+class ProductHandler
+{
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+};
+
+class ProductOutput
+{
+    public function show() {}
+    public function print() {}
+};

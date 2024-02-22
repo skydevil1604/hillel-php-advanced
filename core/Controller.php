9b35777634b32f56236bd2e10f91614d88d41f40
Valeriia <valeriia.kozhevnikova@develit.se>
2024-02-22 03:50:43 +0200

    added MVC router

---
+++ core/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Core;
+
+class Controller
+{
+    public function before(string $action, array $params = []): bool
+    {
+        return true;
+    }
+
+    public function after(string $action) {}
+
+    protected function response(int $code = 200, array $body = [], array $errors = []): array
+    {
+        return compact('code', 'body', 'errors');
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'OLOLO';
+use Core\Router;
+
+define('BASE_DIR', dirname(__DIR__));
+
+require_once BASE_DIR . '/vendor/autoload.php';
+
+$router = Router::getInstance();
+
+try {
+    die($router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']));
+} catch (Exception $exception) {
+    dd($exception);
+}
